2238a24c2e411a599137901dc9e3426874a0ac22
Dylan Steele <dylansteele57@gmail.com>
2025-07-22 23:12:42 -0400

    updated naming & setup jest test runner

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Puttr
+# Angular Material TailwindCSS Starter
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.1.
 
---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+
+// If you want to use paths from tsconfig, enable resolveJsonModule in tsconfig, or just hardcode for now
+
+const config: Config = {
+	preset: "jest-preset-angular",
+	transform: {
+		"^.+\\.(ts|js|mjs|html|svg)$": [
+			"jest-preset-angular",
+			{
+				tsconfig: "<rootDir>/tsconfig-esm.spec.json",
+				stringifyContentPathRegex: "\\.(html|svg)$",
+				useESM: true,
+			},
+		],
+	},
+	setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+	testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+};
+
+export default config;
