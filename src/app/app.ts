1e411265d80ea2a28d7dbcdaab6d2b7c957897a3
Dylan Steele <dylansteele57@gmail.com>
2025-09-20 16:20:13 -0400

    Biome out -> Eslint + Prettier In (#2)
    * swap out biome for eslint + prettier, biome does not support template checking for Ang
    
    * prettier on whole repo
    
    * added basic example component to show Ang Mat + tailwindcss is working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from "@angular/core";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { RouterOutlet } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterOutlet } from '@angular/router';
+import { ExampleComponent } from './components/example/example.component';
 
 @Component({
-	selector: "app-root",
-	imports: [RouterOutlet, MatSlideToggleModule],
-	templateUrl: "./app.html",
-	styleUrl: "./app.scss",
+  selector: 'app-root',
+  imports: [RouterOutlet, MatSlideToggleModule, ExampleComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
 export class App {
-	protected readonly title = signal("angular-mat-tailwind-starter");
+  protected readonly title = signal('angular-mat-tailwind-starter');
 }
