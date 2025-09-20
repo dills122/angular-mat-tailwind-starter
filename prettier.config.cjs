4291462817fb8a8760423446d980e1ae8d9fa1bd
Dylan Steele <dylansteele57@gmail.com>
2025-09-20 14:23:07 -0400

    prettier on whole repo

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -8,15 +8,15 @@ module.exports = {
   useTabs: false,
 
   // Trailing commas where valid in ES5 (objects, arrays, etc.)
-  trailingComma: "es5",
+  trailingComma: 'es5',
 
   // Keep consistent arrow function style
-  arrowParens: "always",
+  arrowParens: 'always',
 
   // Angular/HTML specifics
-  htmlWhitespaceSensitivity: "ignore",
+  htmlWhitespaceSensitivity: 'ignore',
   bracketSameLine: false,
 
   // Ensure formatting applies consistently
-  endOfLine: "lf",
+  endOfLine: 'lf',
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
-import { Component, signal } from "@angular/core";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { RouterOutlet } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-	selector: "app-root",
-	imports: [RouterOutlet, MatSlideToggleModule],
-	templateUrl: "./app.html",
-	styleUrl: "./app.scss",
+  selector: 'app-root',
+  imports: [RouterOutlet, MatSlideToggleModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
 export class App {
-	protected readonly title = signal("angular-mat-tailwind-starter");
+  protected readonly title = signal('angular-mat-tailwind-starter');
 }
